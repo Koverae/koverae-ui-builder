d8c27452e84ba2eaaa5ff1986c3616d9e37772aa
Arden28 <laudbouetoumoussa@gmail.com>
2024-09-29 17:39:15 +0100

    Updated composer file

--- src/KoveraeUiBuilderServiceProvider.php
+++ src/KoveraeUiBuilderServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Koverae\KoveraeUiBuilder;
 
 use Illuminate\Support\ServiceProvider;
+use Koverae\KoveraeUiBuilder\Commands\MakeFormCommand;
 
 class KoveraeUiBuilderServiceProvider extends ServiceProvider
 {
@@ -40,7 +41,9 @@ class KoveraeUiBuilderServiceProvider extends ServiceProvider
             ], 'lang');*/
 
             // Registering package commands.
-            // $this->commands([]);
+            $this->commands([
+                MakeFormCommand::class,
+            ]);
         }
     }
 

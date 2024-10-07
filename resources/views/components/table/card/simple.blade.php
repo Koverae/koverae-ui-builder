96c1f0daf93d803cf3dc186f6f8a5497b06089bf
Arden28 <laudbouetoumoussa@gmail.com>
2024-10-07 09:43:38 +0100

    Updated kanban view in Table component

--- resources/views/components/table/card/simple.blade.php
+++ resources/views/components/table/card/simple.blade.php
@@ -10,7 +10,7 @@
             <img src="{{ $model['avatar'] ? Storage::url('avatars/' . $model['avatar']) . '?v=' . time() : asset('assets/images/default/user.png') }}" alt="{{ $model['avatar'] }}" class="img img-fluid" height="120px" width="120px">
             <div class="p-2 card-body text-truncate">
                 <h5 class="mb-2 card-title"> {{ $model[$value->title] }} </h5>
-                @foreach($this->datas as $data)
+                @foreach($this->data as $data)
                 <span class="mb-1 cursor-pointer text-truncate w-100">{{ $model[$data] }}</span> <br>
                 @endforeach
                 {{-- <a href="#" class="btn btn-primary">Go somewhere</a> --}}
